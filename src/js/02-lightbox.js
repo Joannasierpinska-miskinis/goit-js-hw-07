60d00ba76ce8956abb9c8c5b67983acae21dc986
Joannasierpinska-miskinis <joannasierpinska@gmail.com>
2022-08-04 09:09:40 +0200

    Update 02-lightbox.js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryItem = galleryItems.map(element => {
+    const image = `<a class = "gallery_item" href = "${element.original}" >
+    <img 
+    class= "gallery_image"
+    src = "${element.preview}"
+    alt = "${element.description}"
+    title = "${element.description}"
+    />
+    </a>`;
+    return image;
+})
+    .join(" ");
+gallery.insertAdjacentHTLM("afterbegin", galleryItem);
+
+new SimpleLightbox(".gallery a", { captionDelay: 250 });
 
 console.log(galleryItems);
