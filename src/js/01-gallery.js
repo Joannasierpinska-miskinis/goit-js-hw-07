4bfa3ba4d90ba6c4bd75415af6f226f92477fe7d
Joannasierpinska-miskinis <joannasierpinska@gmail.com>
2022-08-04 08:53:46 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems, galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryItem = galleryItems.map(element => {
+    const image = `<div class = "gallery_item">
+    <a class= "gallery_link" href = "${element.original}">
+    <img 
+    class= "gallery_image" 
+    src= "${element.preview}"
+    data-source= "${element.original} "
+    alt="${element.description}"
+    />
+    </a>
+    </div>`;
+    return image;
+})
+    .join(" ");
+gallery.insertAdjacentHTML("afterbegin", galleryItem);
+gallery.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") return;
+
+
+    const dataImg = event.target.getAttribute("data-source");
+    const altImg = event.target.getAttribute("alt");
+    const instance = basicLightbox.create(`<img src = "${dataImg}" alt = "${altImg}" />`,
+        {
+            onShow: (instance) => {
+                document.addEventListener("keydown", (event) => {
+                    if (event.key === "Escape") instance.close();
+                })
+            }
+        
+        });
+    instance.show();
+});
 
 console.log(galleryItems);
